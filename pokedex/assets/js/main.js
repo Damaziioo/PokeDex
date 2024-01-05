ce21614cc7741178d0137d96e0195119bf5824ca
Lucas Damazio <lucas.damaziioo@gmail.com>
2024-01-04 22:19:30 -0300

    Ajustes e melhorias

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,27 +1,41 @@
 
-function convetPokemonToHtmlLi(pokemon) {
-    return`
-    <li class="pokemon" >
-        <span class="number">${pokemon.order}</span>
-        <span class="name">${pokemon.name}</span>
+const loadMore = document.getElementById('loadMore');
+const maxRecords = 12;
+const limit = 5;
+let offset = 0;
 
-        <div class="detail">
-            <ol class="types"> 
-                ${convetPokemonTypesToHtmlLi(pokemon.types).join('')}
-            </ol>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
-        </div>
-    </li>`
-    
+
+
+function loadMoreItems(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        pokemonList.innerHTML += pokemons.map((pokemon) => `
+        <li class="pokemon ${pokemon.type}" >
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types"> 
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join("")}
+                </ol>
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
+            </div>
+        </li>`).join("")
+    })
+        .catch((error) => console.error(error))
 }
+loadMoreItems()
+loadMore.addEventListener('click', () => {
+    offset += limit
+    debugger
+    const manyRecord = offset + limit;
 
-function convetPokemonTypesToHtmlLi(pokemonTypes) {
-    return pokemonTypes.map((typeSlot)=> `<li class="type">${typeSlot.type.name}</li>`)
-    
-}
+    if (manyRecord >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadMoreItems(offset, newLimit)
 
-const pokemonList = document.getElementById('pokemonList');
+        loadMore.parentElement.removeChild(loadMore)
+    }
+    else {
+        loadMoreItems(offset, limit)
+    }
 
-pokeApi.getPokemons()
-.then((pokemons)=>{pokemonList.innerHTML += pokemons.map(convetPokemonToHtmlLi).join('')})
-.catch((error)=>console.error(error))
+})
